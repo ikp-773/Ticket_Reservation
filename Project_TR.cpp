3f8987fea1b96395ea6111b8d4f2f102d411b63f
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2020-09-26 22:48:58 +0530

    Spacing Function
    Added a new function named Spacing replacing unneccesary '\n' & '\t' for making the code more readable and reusable.

--- Project_TR.cpp
+++ Project_TR.cpp
@@ -5,118 +5,164 @@
 using namespace std;
 int main_screen();
 int snd_screen(int);
+int spacing(int);
 int ticket_booking();
 int ticket_cancelation();
+int thankyou();
 int main(){
 	int i;
 	main_screen();
-	printf("\n\n\n\t\t\t\tChoose Option (1/2/3/4/5) : ");
+	spacing(2);
+	printf("Choose Option (1/2/3/4/5) : ");
 	scanf("%d", &i);
 	snd_screen(i);
   	return 0;	
 }
 int main_screen(){
-	printf("\n\n\t\t\t\t\t*********************************");
-	printf("\n\n\t\t\t\t\t\tTrain Reservation");
-	printf("\n\n\t\t\t\t\t*********************************");
-	printf("\n\n\n\t\t\t\t--> Book Ticket");
-	printf("\n\n\t\t\t\t--> Cancel Ticket");
-	printf("\n\n\t\t\t\t--> Train Database");
-	printf("\n\n\t\t\t\t--> Help");
-	printf("\n\n\t\t\t\t--> Exit");
+	spacing(1);
+	spacing(2);
+	printf("\tTicket Reservation\n");
+	spacing(1);
+	spacing(2);
+	printf("--> Book Ticket");
+	spacing(2);
+	printf("--> Cancel Ticket");
+	spacing(2);
+	printf("--> Train Database");
+	spacing(2);
+	printf("--> Help");
+	spacing(2);
+	printf("--> Exit");
 	return 0;	
 }
 int snd_screen(int input){
 	switch(input){
-		case 1: for(int i=0;i<5;i++)
-					printf("\t   ");
-				printf(" Your Choice --> ");
+		case 1: for(int i=0;i<=5;i++)
+					printf("\t    ");
+				printf("Your Choice --> ");
 				printf("%d",input);
-				sleep(2);
+				sleep(1.5);
 				system("cls");
 				for(int i=0;i<10;i++)
 					printf("\n\t\t\t\t   ");
 				printf("--------------REDIRECTING---------------");
-				sleep(2);
+				sleep(1.5);
 				system("cls");
 				ticket_booking();
 				break;
-		case 2: for(int i=0;i<5;i++)
-					printf("\t   ");
-				printf(" Your Choice --> ");
+		case 2: for(int i=0;i<=5;i++)
+					printf("\t    ");
+				printf("Your Choice --> ");
 				printf("%d",input);
-				sleep(2);
+				sleep(1.5);
 				system("cls");
 				for(int i=0;i<10;i++)
 					printf("\n\t\t\t\t   ");
 				printf("--------------REDIRECTING---------------");
-				sleep(2);
+				sleep(1.5);
 				system("cls");
 				ticket_cancelation();
 				break;
-		case 3: for(int i=0;i<5;i++)
-					printf("\t   ");
-				printf(" Your Choice --> ");
+		case 3: for(int i=0;i<=5;i++)
+					printf("\t    ");
+				printf("Your Choice --> ");
 				printf("%d",input);
-				sleep(2);
+				sleep(1.5);
 				system("cls");
 				for(int i=0;i<10;i++)
 					printf("\n\t\t\t\t   ");
 				printf("--------------REDIRECTING---------------");
-				sleep(2);
+				sleep(1.5);
 				system("cls");
 				break;
-		case 4: for(int i=0;i<5;i++)
-					printf("\t   ");
-				printf(" Your Choice --> ");
+		case 4: for(int i=0;i<=5;i++)
+					printf("\t    ");
+				printf("Your Choice --> ");
 				printf("%d",input);
-				sleep(2);
+				sleep(1.5);
 				system("cls");
 				for(int i=0;i<10;i++)
 					printf("\n\t\t\t\t   ");
 				printf("--------------REDIRECTING---------------");
-				sleep(2);
+				sleep(1.5);
 				system("cls");
 				break;
-		case 5: for(int i=0;i<5;i++)
-					printf("\t   ");
-				printf(" Your Choice --> ");
+		case 5: for(int i=0;i<=5;i++)
+					printf("\t    ");
+				printf("Your Choice --> ");
 				printf("%d",input);
-				sleep(2);
+				sleep(1.5);
 				system("cls");
 				for(int i=0;i<10;i++)
 					printf("\n\t\t\t\t   ");
 				printf("--------------REDIRECTING---------------");
-				sleep(2);
+				sleep(1.5);
 				system("cls");
+				thankyou();
 				break;
 		default:printf("\n");
-				for(int i=0;i<5;i++)
+				for(int i=0;i<=5;i++)
 					printf("\t   "); 
 				printf("------INVALID INPUT-------");
-				sleep(2);
+				sleep(1.5);
+	}
+}
+int spacing(int value){
+	int i;
+    switch(value){
+		case 1: for(i=0;i<3;i++)
+					printf("\n");
+				for(i=0;i<5;i++)	
+					printf("\t");
+				for(i=0;i<=32;i++)
+					printf("*");
+		case 2: for(i=0;i<2;i++)
+					printf("\n");
+				for(i=0;i<5;i++)
+					printf("\t");
 	}
 }
 int ticket_booking(){
-	printf("\n\n\t\t\t\t\t*********************************");
-	printf("\n\n\t\t\t\t\t\tTicket Booking");
-	printf("\n\n\t\t\t\t\t*********************************");
-	printf("\n\n\n\t\t\t\t--> Book Ticket");
-	printf("\n\n\t\t\t\t--> Review Ticket");
-	printf("\n\n\t\t\t\t--> Ticket Chart");
-	printf("\n\n\t\t\t\t--> Go Back");
-	printf("\n\n\t\t\t\t--> Exit");
+	spacing(1);
+	spacing(2);
+	printf("\t  Ticket Booking\n");
+	spacing(1);
+	spacing(2);
+	printf("--> Book Ticket");
+	spacing(2);
+	printf("--> Review Ticket");
+	spacing(2);
+	printf("--> Ticket Chart");
+	spacing(2);
+	printf("--> Go Back");
+	spacing(2);
+	printf("--> Exit");
 	return 0;
 }
 int ticket_cancelation(){
-	printf("\n\n\t\t\t\t\t*********************************");
-	printf("\n\n\t\t\t\t\t\tTicket Cancellation");
-	printf("\n\n\t\t\t\t\t*********************************");
-	printf("\n\n\n\t\t\t\t--> Cancel Ticket");
-	printf("\n\n\t\t\t\t--> Review Ticket");
-	printf("\n\n\t\t\t\t--> Ticket Chart");
-	printf("\n\n\t\t\t\t--> Go Back");
-	printf("\n\n\t\t\t\t--> Exit");
+	spacing(1);
+	spacing(2);
+	printf("\t\tTicket Cancellation");
+	spacing(1);
+	spacing(2);
+	printf("--> Cancel Ticket");
+	spacing(2);
+	printf("--> Review Ticket");
+	spacing(2);
+	printf("--> Ticket Chart");
+	spacing(2);
+	printf("--> Go Back");
+	spacing(2);
+	printf("--> Exit");
 	return 0;
+}
+int thankyou(){
+	spacing(1);
+	spacing(2);
+	printf("\t\t  Thank You!");
+	spacing(2);
+	printf("\t\t  Visit Again");
+	spacing(1);
+	exit(0);
 }
 
