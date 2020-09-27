02e246a8305a7a11118f9e7942e58be661709387
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2020-09-27 14:03:42 +0530

    New Addition
    Added Databse Main Screen
    Added Help Main Screen
    Added Input Function for Accepting Input
    Corrected the Default Case in Switch

--- Project_TR.cpp
+++ Project_TR.cpp
@@ -1,21 +1,22 @@
+// Last Updated : 27-09-20 13:53
 #include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<conio.h>
 using namespace std;
-int main_screen();
-int snd_screen(int);
-int spacing(int);
-int ticket_booking();
-int ticket_cancelation();
-int thankyou();
+int main_screen();//Prototype for Main Screen
+int snd_screen(int);//Prototype for Input Screen
+int input_fn(int);//Prototype for Input Function
+int spacing(int);//Prototype for Spacing Function
+int ticket_booking();//Prototype fot Booking Ticket
+int ticket_cancelation();//Prototype for Cancelling Ticket
+int database();
+int help();
+int thankyou();//Prototype for ThankYou Screen
+int i;//Loop Variable
 int main(){
-	int i;
 	main_screen();
-	spacing(2);
-	printf("Choose Option (1/2/3/4/5) : ");
-	scanf("%d", &i);
-	snd_screen(i);
   	return 0;	
 }
 int main_screen(){
@@ -28,101 +29,71 @@ int main_screen(){
 	spacing(2);
 	printf("--> Cancel Ticket");
 	spacing(2);
-	printf("--> Train Database");
+	printf("--> Database");
 	spacing(2);
 	printf("--> Help");
 	spacing(2);
 	printf("--> Exit");
+	spacing(2);
+	printf("Choose Option (1/2/3/4/5) : ");
+	scanf("%d", &i);
+	snd_screen(i);
 	return 0;	
 }
 int snd_screen(int input){
 	switch(input){
-		case 1: for(int i=0;i<=5;i++)
-					printf("\t    ");
-				printf("Your Choice --> ");
-				printf("%d",input);
-				sleep(1.5);
-				system("cls");
-				for(int i=0;i<10;i++)
-					printf("\n\t\t\t\t   ");
-				printf("--------------REDIRECTING---------------");
-				sleep(1.5);
-				system("cls");
+		case 1: input_fn(input);
 				ticket_booking();
 				break;
-		case 2: for(int i=0;i<=5;i++)
-					printf("\t    ");
-				printf("Your Choice --> ");
-				printf("%d",input);
-				sleep(1.5);
-				system("cls");
-				for(int i=0;i<10;i++)
-					printf("\n\t\t\t\t   ");
-				printf("--------------REDIRECTING---------------");
-				sleep(1.5);
-				system("cls");
+		case 2: input_fn(input);
 				ticket_cancelation();
 				break;
-		case 3: for(int i=0;i<=5;i++)
-					printf("\t    ");
-				printf("Your Choice --> ");
-				printf("%d",input);
-				sleep(1.5);
-				system("cls");
-				for(int i=0;i<10;i++)
-					printf("\n\t\t\t\t   ");
-				printf("--------------REDIRECTING---------------");
-				sleep(1.5);
-				system("cls");
+		case 3: input_fn(input);
+				database();
 				break;
-		case 4: for(int i=0;i<=5;i++)
-					printf("\t    ");
-				printf("Your Choice --> ");
-				printf("%d",input);
-				sleep(1.5);
-				system("cls");
-				for(int i=0;i<10;i++)
-					printf("\n\t\t\t\t   ");
-				printf("--------------REDIRECTING---------------");
-				sleep(1.5);
-				system("cls");
+		case 4: input_fn(input);
+				help();
 				break;
-		case 5: for(int i=0;i<=5;i++)
-					printf("\t    ");
-				printf("Your Choice --> ");
-				printf("%d",input);
-				sleep(1.5);
-				system("cls");
-				for(int i=0;i<10;i++)
-					printf("\n\t\t\t\t   ");
-				printf("--------------REDIRECTING---------------");
-				sleep(1.5);
-				system("cls");
+		case 5: input_fn(input);
 				thankyou();
 				break;
 		default:printf("\n");
-				for(int i=0;i<=5;i++)
+				for(i=0;i<=5;i++)
 					printf("\t   "); 
 				printf("------INVALID INPUT-------");
 				sleep(1.5);
 	}
+	return 0;
 }
 int spacing(int value){
-	int i;
     switch(value){
-		case 1: for(i=0;i<3;i++)
+		case 1: for(i=0;i<3;i++)//Heading Spacing
 					printf("\n");
 				for(i=0;i<5;i++)	
 					printf("\t");
 				for(i=0;i<=32;i++)
 					printf("*");
-		case 2: for(i=0;i<2;i++)
+		case 2: for(i=0;i<2;i++)//Paragraph Spacing
 					printf("\n");
 				for(i=0;i<5;i++)
 					printf("\t");
+	return 0;
 	}
 }
-int ticket_booking(){
+int input_fn(int input_value){//Function for showing input
+	for(i=0;i<=5;i++)
+		printf("\t    ");
+	printf("Your Choice --> ");
+	printf("%d",input_value);
+	sleep(1.5);
+	system("cls");
+	for(i=0;i<10;i++)
+		printf("\n\t\t\t\t   ");
+	printf("--------------REDIRECTING---------------");
+	sleep(1.5);
+	system("cls");
+}
+int ticket_booking(){//Fuction for Ticket Booking Screen
 	spacing(1);
 	spacing(2);
 	printf("\t  Ticket Booking\n");
@@ -137,12 +108,36 @@ int ticket_booking(){
 	printf("--> Go Back");
 	spacing(2);
 	printf("--> Exit");
+	spacing(2);
+	printf("Choose Option (1/2/3/4/5) : ");
+	scanf("%d", &i);
+	switch(i){
+		case 1: input_fn(i);
+				break;
+		case 2: input_fn(i);
+				break;
+		case 3: input_fn(i);
+				break;
+		case 4: input_fn(i);
+				main_screen();
+				break;
+		case 5: input_fn(i);
+				thankyou();
+				break;
+		default:printf("\n");
+				for(i=0;i<=5;i++)
+					printf("\t   "); 
+				printf("------INVALID INPUT-------");
+				sleep(2);
+				system("cls");
+				ticket_booking();
+	}
 	return 0;
 }
-int ticket_cancelation(){
+int ticket_cancelation(){//Fuction for Ticket Cancellation Screen
 	spacing(1);
 	spacing(2);
-	printf("\t\tTicket Cancellation");
+	printf("\tTicket Cancellation\n");
 	spacing(1);
 	spacing(2);
 	printf("--> Cancel Ticket");
@@ -154,14 +149,90 @@ int ticket_cancelation(){
 	printf("--> Go Back");
 	spacing(2);
 	printf("--> Exit");
+	spacing(2);
+	printf("Choose Option (1/2/3/4/5) : ");
+	scanf("%d", &i);
+	switch(i){
+		case 1: input_fn(i);
+				break;
+		case 2: input_fn(i);
+				break;
+		case 3: input_fn(i);
+				break;
+		case 4: input_fn(i);
+				main_screen();
+				break;
+		case 5: input_fn(i);
+				thankyou();
+				break;
+		default:printf("\n");
+				for(i=0;i<=5;i++)
+					printf("\t   "); 
+				printf("------INVALID INPUT-------");
+				sleep(2);
+				system("cls");
+				ticket_cancelation();
+	}
 	return 0;
 }
-int thankyou(){
+int database(){
 	spacing(1);
 	spacing(2);
-	printf("\t\t  Thank You!");
+	printf("\tDatabase\n");
+	spacing(1);
 	spacing(2);
-	printf("\t\t  Visit Again");
+	printf("--> User Database");
+	spacing(2);
+	printf("--> Admin Databse");
+	spacing(2);
+	printf("--> Travel Database");
+	spacing(2);
+	printf("--> Go Back");
+	spacing(2);
+	printf("--> Exit");
+	spacing(2);
+	printf("Choose Option (1/2/3/4/5) : ");
+	scanf("%d", &i);
+	switch(i){
+		case 1: input_fn(i);
+				break;
+		case 2: input_fn(i);
+				break;
+		case 3: input_fn(i);
+				break;
+		case 4: input_fn(i);
+				main_screen();
+				break;
+		case 5: input_fn(i);
+				thankyou();
+				break;
+		default:printf("\n");
+				for(i=0;i<=5;i++)
+					printf("\t   "); 
+				printf("------INVALID INPUT-------");
+				sleep(2);
+				system("cls");
+				database();
+	}
+	return 0;
+}
+int help(){
+	spacing(1);
+	spacing(2);
+	printf("\t     Help\n");
+	spacing(1);
+	spacing(2);
+	printf("Press Enter to Go Back.....");
+	getch();
+	system("cls");
+	main_screen();
+}
+int thankyou(){//Function for ThankYou Screen
+	spacing(1);
+	spacing(2);
+	printf("\t   Thank You!");
+	spacing(2);
+	printf("\t   Visit Again\n");
 	spacing(1);
 	exit(0);
 }
